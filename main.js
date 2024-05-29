765d9637fc03a260bcc67c8ad68f2ab68e42bdd5
Emilly <103767720+emymirada@users.noreply.github.com>
2024-05-29 09:40:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var cxPrincipal = document.querySelector("cx-principal");
+var cxPerguntas = document.querySelector("cx-perguntas");
+var cxAlternativas = document.querySelector("cx-alternativas");
+var cxResultados = document.querySelector("cx-resultdos");
+var txResultados = document.querySelector("tx-resultdos");
+var perguntas = [
+    {
+        enunciado : "A IA pode automatizar tarefas repetitivas",
+        alternativas : [
+            "Vai eliminar tarefas monotonas",
+            "Vai tirar muitos empregos"
+        ]
+    },
+    {
+        enunciado : "",
+        alternativas : [
+            "",
+            ""
+        ]
+    },
+    {
+        enunciado : "",
+        alternativas : [
+            "",
+            ""
+        ]
+    },
+    {
+        enunciado : "",
+        alternativas : [
+            "",
+            ""
+        ]
+    },
+    {
+        enunciado : "",
+        alternativas : [
+            "",
+            ""
+        ]
+    }
+]
+
+
+
