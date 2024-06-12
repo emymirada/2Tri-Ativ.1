0ee241841b23c855ec0729725fac20a556bf3064
Emilly <103767720+emymirada@users.noreply.github.com>
2024-06-12 08:14:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,53 @@
-var cxPrincipal = document.querySelector("cx-principal");
-var cxPerguntas = document.querySelector("cx-perguntas");
-var cxAlternativas = document.querySelector("cx-alternativas");
-var cxResultados = document.querySelector("cx-resultdos");
-var txResultados = document.querySelector("tx-resultdos");
+var cxPrincipal = document.querySelector(".cx-principal");
+var cxPerguntas = document.querySelector(".cx-perguntas");
+var cxAlternativas = document.querySelector(".cx-alternativas");
+var cxResultado = document.querySelector(".cx-resultado");
+var txResultado = document.querySelector(".tx-resultado");
 var perguntas = [
     {
-        enunciado : "A IA pode automatizar tarefas repetitivas",
-        alternativas : [
+        enunciado: "A IA pode automatizar tarefas repetitivas.",
+        alternativas: [
             "Vai eliminar tarefas monotonas",
             "Vai tirar muitos empregos"
         ]
     },
     {
-        enunciado : "",
-        alternativas : [
-            "",
-            ""
+        enunciado: "A IA pode tomar decisões baseadas em dados.",
+        alternativas: [
+            "Maravilhoso",
+            "Assustador"
         ]
     },
     {
-        enunciado : "",
-        alternativas : [
-            "",
-            ""
+        enunciado: "A IA pode substituir certos empregos.",
+        alternativas: [
+            "Não vejo problemas",
+            "É o fim dos empregos como conhecemos hoje"
         ]
     },
     {
-        enunciado : "",
-        alternativas : [
-            "",
-            ""
+        enunciado: "A IA pode ajudar a prever tendências de mercado.",
+        alternativas: [
+           
+            "Incrível",
+            "Sinistro"
         ]
     },
     {
-        enunciado : "",
-        alternativas : [
-            "",
-            ""
+        enunciado: "A IA pode trabalhar 24/7 sem pausas.",
+        alternativas: [
+            "Surpreendente",
+            "Mão de obra barata"
         ]
     }
-]
+];
 
+var posiçãoAtual = 0;
+var perguntaAtual;
 
+mostrarPergunta();
 
+function mostrarPergunta(){
+    perguntaAtual = perguntas[posiçãoAtual];
+    cxPerguntas.textContent = perguntaAtual.enunciado;
+}
